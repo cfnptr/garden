0af153bb17b11dc45e21693c71688838eacb8efc
Nikita Fediuchin <cfnptr@gmail.com>
2025-01-08 21:14:58 +0200

    Happy New Year! 2025

--- include/garden/system/render/atmosphere.hpp
+++ include/garden/system/render/atmosphere.hpp
@@ -1,5 +1,4 @@
-//--------------------------------------------------------------------------------------------------
-// Copyright 2022-2024 Nikita Fediuchin. All rights reserved.
+// Copyright 2022-2025 Nikita Fediuchin. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,10 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
-// Based on this paper: https://sebh.github.io/publications/egsr2020.pdf
-// One of implementations: https://www.shadertoy.com/view/slSXRW
-//--------------------------------------------------------------------------------------------------
+
+/***********************************************************************************************************************
+ * @file
+ * @brief TODO
+ * 
+ * @details
+ * Based on this paper: https://sebh.github.io/publications/egsr2020.pdf
+ * One of implementations: https://www.shadertoy.com/view/slSXRW
+ */
 
 /*
 #pragma once
